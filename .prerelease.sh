a6c4a509032217448c11aff3d54cd4dd8a2afe56
Jason Chu <me@chuie.io>
2021-02-20 23:17:39 -0800

    fix: better go get caching

--- .prerelease.sh
+++ .prerelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sleep 20 # allow github to catch up
 cd casket
-GOPROXY=direct go get "github.com/tmpim/casket@master"
+GOPRIVATE=github.com/tmpim/casket go get "github.com/tmpim/casket@master"
 go mod tidy
 cd ..
